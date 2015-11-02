5bc6998b1cef98a875463e9b780e0f0d99ce25d3
Jannis <jannisma@web.de>
2015-11-02 21:24:56 +0100

    add istype methods to boardobject

--- AntWars/Board/BoardObject.cs
+++ AntWars/Board/BoardObject.cs
@@ -10,10 +10,40 @@ namespace AntWars
     {
         public int x { get; set; }
         public int y { get; set; }
-        public Boolean signalPlayer1 { get; set; }
-        public Boolean signalPlayer2 { get; set; }
+        public bool signalPlayer1 { get; set; }
+        public bool signalPlayer2 { get; set; }
 
+        public bool isAnt()
+        {
+            return this.GetType() == typeof(Ant);
+        }
 
+        public bool isSugar()
+        {
+            return this.GetType() == typeof(Sugar);
+        }
 
+        public bool isBase()
+        {
+            return this.GetType() == typeof(Base);
+        }
+
+        public Ant getAnt()
+        {
+            if (!isAnt()) { return null; }
+            return (Ant)this;
+        }
+
+        public Base getBase()
+        {
+            if (!isBase()) { return null; }
+            return (Base)this;
+        }
+
+        public Sugar getSugar()
+        {
+            if (!isSugar()) { return null; }
+            return (Sugar)this;
+        }
     }
 }
