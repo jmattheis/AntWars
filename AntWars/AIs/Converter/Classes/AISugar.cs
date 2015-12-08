eaf916e39b79a5b85e6131499ff1459a8135e601
nicolasries <nicolas.ries@t-online.de>
2015-12-08 12:09:46 +0100

    Grundgeruest fuer AI-Konvertierung einbauen

---
+++ AntWars/AIs/Converter/Classes/AISugar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntWars.Board;
+
+namespace AntWars.AIs.Converter.Classes
+{
+    class AISugar : AIBoardObject
+    {
+        public AISugar(Sugar sugar)
+            : base(sugar)
+        {
+
+        }
+    }
+}
