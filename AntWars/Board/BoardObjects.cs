4426164736a85eae59a70780a431aff46a6c2f42
Jannis <jannisma@web.de>
2015-12-10 12:23:03 +0100

    add boardobjects

--- AntWars/Board/BoardObjects.cs
+++ AntWars/Board/BoardObjects.cs
@@ -14,6 +14,8 @@ namespace AntWars.Board
         private List<Signal> signals = new List<Signal>();
         private List<Base> bases = new List<Base>();
         private List<Sugar> sugars = new List<Sugar>();
+        Dictionary<Coordinates, List<BoardObject>> coordsToObjects;
+        
         public void add(BoardObject boardObject)
         {
             boardObjects.Add(boardObject);
@@ -33,7 +35,55 @@ namespace AntWars.Board
             {
                 sugars.Add((Sugar)boardObject);
             }
+            addToMap(boardObject);
+            
+        }
+        
+        private void addToMap(BoardObject boardObject) 
+        {
+          List<BoardObject> objsInCoords
+          if (!dic.TryGetValue(boardObject.Coords, out objsInCoords)) 
+          {
+            objsInCoords = new List<BoardObject>();
+            dic.Add(boardObject.Coords, objsInCoords);
+          }
+          objsInCoords.Add(boardObject);
         }
 
+        public List<BoardObject> get()
+        {
+          return new ReadOnlyCollection<Item>(boardObjects);
+        }
+        
+        public List<Ant> getAnts()
+        {
+          return new ReadOnlyCollection<Item>(ants);
+        }
+        
+        public List<Sugar> getSugars() 
+        {
+          return new ReadOnlyCollection<Item>(sugars); 
+        }
+        
+        public List<Signal> getSignals()
+        {
+          return new ReadOnlyCollection<Item>(signals);
+        }
+        
+        public List<Base> getBases() {
+        {
+          return new ReadOnlyCollection<Item>(bases);
+        }
+        
+        public void move(BoardObject obj, Coordinates coords) {
+        {
+          List<BoardObject> objsInCoords
+          if (!dic.TryGetValue(boardObject.Coords, out objsInCoords)) {
+            throw new RuntimeException("No found");
+          }
+          objsInCoords.Remove(obj);
+          obj.Coords = coords;
+          addToMap(obj);
+        }
     }
 }
