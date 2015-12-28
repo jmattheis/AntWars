45125eaa4a36d98e203127af5166a14e061f97c0
nicolasries <nicolas.ries@t-online.de>
2015-12-28 11:42:31 +0100

    Zucker aufheben und verschwinden lassen Closes #15

--- AntWars/AIs/Converter/Classes/AIAnt.cs
+++ AntWars/AIs/Converter/Classes/AIAnt.cs
@@ -47,5 +47,47 @@ namespace AntWars.AIs.Converter.Classes
             return board.BoardObjects.move(ant, newCoords);
         }
 
+        /// <summary>
+        /// Zucker aufnehmen. Die Ameise muss auf dem Zucker stehen.
+        /// </summary>
+        /// <returns></returns>
+        public bool pickUpSugar()
+        {
+            Sugar sugar;
+            if (this.board.BoardObjects.getSugar(this.Coords, out sugar) && ant.Inventory > 0)
+            {
+                int temp = sugar.amount;
+                if (sugar.amount - ant.Inventory <= 0)
+                {
+                    // Zucker bei 0 entfernen
+                    sugar.amount = 0;
+                    this.board.BoardObjects.remove(sugar);
+                }
+                else
+                    sugar.amount = sugar.amount - ant.Inventory;
+
+                ant.Inventory -= (temp - sugar.amount);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Zucker bei der Base abgeben. Die Ameise muss auf der Base stehen.
+        /// </summary>
+        /// <returns></returns>
+        public bool dropSugarOnBase()
+        {
+            if (this.Coords == this.board.BoardObjects.getBase(this.Owner).Coords)
+            {
+                this.Owner.money += ant.Inventory;
+                this.Owner.Points += ant.Inventory;
+                ant.Inventory = 0;
+                return true;
+            }
+            else
+                return false;
+        }
     }
 }
