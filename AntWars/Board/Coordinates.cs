d48248142c5548303c87f139f002d027b8b0e2b8
Jannis Mattheis <jannis.mattheis@omikron.net>
2016-02-16 12:46:04 +0100

    improve equals performance

--- AntWars/Board/Coordinates.cs
+++ AntWars/Board/Coordinates.cs
@@ -24,31 +24,12 @@ namespace AntWars.Board
 
         public override bool Equals(System.Object obj)
         {
-            // If parameter is null return false.
-            if (obj == null)
-            {
-                return false;
-            }
-
-            // If parameter cannot be cast to Point return false.
-            Coordinates p = obj as Coordinates;
-            if ((System.Object)p == null)
-            {
-                return false;
-            }
-
-            // Return true if the fields match:
-            return (X == p.X) && (Y == p.Y);
+            return Equals(obj as Coordinates);
         }
 
         public bool Equals(Coordinates coords)
         {
-            if ((object)coords == null)
-            {
-                return false;
-            }
-
-            return (X == coords.X) && (Y == coords.Y);
+            return coords != null && X == coords.X && Y == coords.Y;
         }
 
         public override int GetHashCode()
